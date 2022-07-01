436c13cdb1cc503b08c55ab50dda76b938c93664
lucas <lucasregoaraujo@gmail.com>
2022-06-30 22:44:53 -0300

    Exemplo com o tipo Enumerado

--- FirstExercise/Program.cs
+++ FirstExercise/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using FirstExercise.Entities;
+using FirstExercise.Entities.Enums;
+using System;
+
 
 namespace FirstExercise
 {
@@ -8,44 +9,21 @@ namespace FirstExercise
     {
         static void Main(string[] args)
         {
-            Console.Write("How many employees will be registered? ");
-            int n = int.Parse(Console.ReadLine());
-
-            List<Employee> employees = new List<Employee>();
-
-            for(int i = 0; i < n; i++)
+            Order order = new Order
             {
-                Console.WriteLine("\nEmployee #" + (i + 1));
-                Console.Write("Id: ");
-                int id = int.Parse(Console.ReadLine());
-                Console.Write("Name: ");
-                String name = Console.ReadLine();
-                Console.Write("Salary: ");
-                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                employees.Add(new Employee { Id = id, Name = name, Salary = salary });
-            }
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Shipped,
+            };
 
-            Console.Write("\nEnter the employee id that will have salary increase: ");
-            int empId = int.Parse(Console.ReadLine());
-            Employee emp = employees.Find(emp => emp.Id == empId);
-            if(emp != null)
-            {
-                Console.Write("Enter the percentage: ");
-                double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                emp.IncreaseSalary(percent);
-            }
-            else
-            {
-                Console.WriteLine("This id does not exist!");
-            }
+            Console.WriteLine(order);
 
-            Console.WriteLine("\nUpdated list of employees:");
-            foreach(Employee employee in employees)
-            {
-                Console.WriteLine(employee);
-            }
+            string txt = OrderStatus.PendingPayment.ToString();
 
-            
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
         }
     }
 }
